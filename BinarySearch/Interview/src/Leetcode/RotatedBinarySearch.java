dd301a546db423b0a63317df684d4560c3ed6f09
Parshva <parshvadani@gmail.com>
2023-09-26 17:38:06 +0530

    Rotated Binary Search With Duplicat Elements

--- BinarySearch/Interview/src/Leetcode/RotatedBinarySearch.java
+++ BinarySearch/Interview/src/Leetcode/RotatedBinarySearch.java
@@ -22,6 +22,8 @@ public class RotatedBinarySearch {
         }
         return binary(nums,target,pivot+1,nums.length-1);
     }
+
+    //This will not work for duplicate values
     static int findPivot(int [] arr){
         int start = 0;
         int end = arr.length-1;
@@ -38,6 +40,43 @@ public class RotatedBinarySearch {
                 end = mid-1;
             }else{
                 start = mid+1;
+            }
+        }
+        return -1;
+    }
+    static int findPivotWithDuplicates(int [] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end){
+            int mid  = start + (end - start) / 2;
+            // 4 Cases over here
+            if(mid < end  && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid - 1]){
+                return mid-1;
+            }
+            // if elements at middle,start,end are equal just skip the duplicates
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                // skip the duplicates
+                //Note what if the elements at the start and the end were the pivots??
+                //check if start is pivot
+                if (arr[start] > arr[start+1]){
+                    return start;
+                }
+                start++;
+                //check if end is pivot
+                if(arr[end] < arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            }
+            //left side is sorted, so pivot should be in the right
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])){
+                start = mid + 1;
+            }
+            else{
+                end  =mid-1;
             }
         }
         return -1;
