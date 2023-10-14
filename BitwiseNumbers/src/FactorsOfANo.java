2f93f9ae0a612dbadf370382c3ca61445de508e3
parshva555 <parshvadani@gmail.com>
2023-10-14 13:34:48 +0530

    Factors Method 2

--- BitwiseNumbers/src/FactorsOfANo.java
+++ BitwiseNumbers/src/FactorsOfANo.java
@@ -1,11 +1,25 @@
 public class FactorsOfANo {
     public static void main(String[] args) {
-        factors1(20);
+        factors2(20);
     }
+
+    // O(n)
     static void factors1(int n){
         for (int i = 1; i <=n ; i++) {
             if(n % i == 0){
                 System.out.print(i + " ");
+            }
+        }
+    }
+    // O(sqrt(n))
+    static void factors2(int n){
+        for (int i = 1; i <=Math.sqrt(n) ; i++) {
+            if(n % i == 0){
+                if(n/i == i){
+                    System.out.println(i);
+                }else{
+                    System.out.println(i + " " + n/i);
+                }
             }
         }
     }
