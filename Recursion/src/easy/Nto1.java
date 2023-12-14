01546a618446e3e27604fe026b3658a4c4fe5967
parshva555 <parshvadani@gmail.com>
2023-12-14 15:10:49 +0530

    Funtion calling in both normal and reverse

--- Recursion/src/easy/Nto1.java
+++ Recursion/src/easy/Nto1.java
@@ -4,6 +4,7 @@ public class Nto1 {
     public static void main(String[] args) {
         fun(5);
         funReverse(5);
+        funBoth(5);
     }
 
     static void fun(int n){
@@ -19,6 +20,13 @@ public class Nto1 {
         }
         funReverse(n-1);
         System.out.println(n);
-
+    }
+    static void funBoth(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
     }
 }
