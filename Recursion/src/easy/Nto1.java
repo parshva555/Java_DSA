aab0842d26b0a388e7d7cedbb18067a51f276869
parshva555 <parshvadani@gmail.com>
2023-12-14 15:08:28 +0530

    Function reverse 1 to N

--- Recursion/src/easy/Nto1.java
+++ Recursion/src/easy/Nto1.java
@@ -3,7 +3,9 @@ package easy;
 public class Nto1 {
     public static void main(String[] args) {
         fun(5);
+        funReverse(5);
     }
+
     static void fun(int n){
         if(n==0){
             return;
@@ -11,5 +13,12 @@ public class Nto1 {
         System.out.println(n);
         fun(n-1);
     }
+    static void funReverse(int n){
+        if(n==0){
+            return;
+        }
+        funReverse(n-1);
+        System.out.println(n);
 
+    }
 }
