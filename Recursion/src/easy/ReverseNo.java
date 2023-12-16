1d39658dd61be4e7d8f7169150fb4def9028dcc2
parshva555 <parshvadani@gmail.com>
2023-12-16 18:05:17 +0530

    Different way to reverse a number using recursion

--- Recursion/src/easy/ReverseNo.java
+++ Recursion/src/easy/ReverseNo.java
@@ -10,9 +10,20 @@ public class ReverseNo {
         sum = sum * 10 + rem;
         rev1(n/10);
     }
+    static int rev2(int n){
+        int digits = (int)(Math.log10(n)) + 1;
+        return helper(n,digits);
+    }
+    private static int helper(int n , int digits){
+        if(n%10 == n){
+            return n;
+        }
+        int rem = n%10;
+        return rem * (int)Math.pow(10,digits-1) + helper(n/10,digits-1);
+    }
 
     public static void main(String[] args) {
-        rev1(1234);
-        System.out.println(sum);
+        System.out.println(rev2(1234));
+
     }
 }
