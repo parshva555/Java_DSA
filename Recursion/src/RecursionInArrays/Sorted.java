a77f186dcf59de0363b5d915317a31de02d9496d
parshva555 <parshvadani@gmail.com>
2023-12-18 15:44:14 +0530

    Check if array is sorted or not using recusion

---
+++ Recursion/src/RecursionInArrays/Sorted.java
@@ -0,0 +1,15 @@
+package RecursionInArrays;
+
+public class Sorted {
+    public static void main(String[] args) {
+        int [] arr  = {1,2,3,5};
+        System.out.println(sortedOrNot(arr,0));
+    }
+    static boolean sortedOrNot(int [] arr, int index){
+        // base condition
+        if(index == arr.length - 1){
+            return true;
+        }
+        return arr[index] < arr[index +1] && sortedOrNot(arr,index+1);
+    }
+}
