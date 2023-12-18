9bb970fc565d0614d6ecda0d9e0323af6d33101d
parshva555 <parshvadani@gmail.com>
2023-12-18 16:06:44 +0530

    To find indexes from last

--- Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
+++ Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
@@ -6,6 +6,8 @@ public class LinearSearchUsingRec {
         int target = 23;
         int ans  = findIndex(arr,target,0);
         System.out.println(ans);
+        int ans1 = findIndexLast(arr,target,arr.length-1);
+        System.out.println(ans1);
     }
     static boolean find(int [] arr, int target, int index){
         if(index == arr.length){
@@ -22,6 +24,17 @@ public class LinearSearchUsingRec {
         }
         else{
             return findIndex(arr,target,index+1);
+        }
+    }
+    static int findIndexLast(int [] arr, int target , int index){
+        if(index == -1){
+            return -1;
+        }
+        if(arr[index] == target) {
+            return index;
+        }
+        else{
+            return findIndexLast(arr,target,index-1);
         }
     }
 }
