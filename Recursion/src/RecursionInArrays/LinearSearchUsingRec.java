3593d8f19e4ddb5d9d67960cf2b91a5945aa26d2
parshva555 <parshvadani@gmail.com>
2023-12-18 16:13:35 +0530

    Linear search for multiple occuirences using recusrion and arrarylist

--- Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
+++ Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
@@ -1,13 +1,17 @@
 package RecursionInArrays;
 
+import java.util.ArrayList;
+
 public class LinearSearchUsingRec {
     public static void main(String[] args) {
-        int[] arr = {123,3452,3,54,23};
-        int target = 23;
+        int[] arr = {123,3452,3,3,54,54,23};
+        int target = 3;
         int ans  = findIndex(arr,target,0);
         System.out.println(ans);
         int ans1 = findIndexLast(arr,target,arr.length-1);
         System.out.println(ans1);
+        findAllIndex(arr,3,0);
+        System.out.println(list);
     }
     static boolean find(int [] arr, int target, int index){
         if(index == arr.length){
@@ -36,5 +40,16 @@ public class LinearSearchUsingRec {
         else{
             return findIndexLast(arr,target,index-1);
         }
+    }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int [] arr, int target , int index){
+        if(index == arr.length){
+            return ;
+        }
+        if(arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr,target,index+1);
     }
 }
