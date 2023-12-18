bccbd0a0ae0c2af62b453b62e572507744e8ed64
parshva555 <parshvadani@gmail.com>
2023-12-18 20:16:02 +0530

    return the list without passing an argument

--- Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
+++ Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
@@ -1,17 +1,20 @@
 package RecursionInArrays;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class LinearSearchUsingRec {
     public static void main(String[] args) {
         int[] arr = {123,3452,3,3,54,54,23};
         int target = 3;
-        int ans  = findIndex(arr,target,0);
-        System.out.println(ans);
-        int ans1 = findIndexLast(arr,target,arr.length-1);
-        System.out.println(ans1);
-        findAllIndex(arr,3,0);
-        System.out.println(list);
+//        int ans  = findIndex(arr,target,0);
+//        System.out.println(ans);
+//        int ans1 = findIndexLast(arr,target,arr.length-1);
+//        System.out.println(ans1);
+////        findAllIndex(arr,3,0);
+////        System.out.println(list);
+//        System.out.println(findAllIndex(arr,3,0,new ArrayList<>()));
+        System.out.println(findAllIndex2(arr,3,0));
     }
     static boolean find(int [] arr, int target, int index){
         if(index == arr.length){
@@ -42,14 +45,27 @@ public class LinearSearchUsingRec {
         }
     }
 
-    static ArrayList<Integer> list = new ArrayList<>();
-    static void findAllIndex(int [] arr, int target , int index){
+
+    static ArrayList<Integer> findAllIndex(int [] arr, int target , int index,ArrayList<Integer> list){
         if(index == arr.length){
-            return ;
+            return list;
         }
         if(arr[index] == target) {
             list.add(index);
         }
-        findAllIndex(arr,target,index+1);
+        return findAllIndex(arr,target,index+1,list);
+    }
+    static ArrayList<Integer> findAllIndex2(int [] arr, int target , int index){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(index == arr.length){
+            return list;
+        }
+        // this is will contain answer for that function call only
+        if(arr[index] == target) {
+            list.add(index);
+        }
+        ArrayList<Integer> answerFromBelowCalls =  findAllIndex2(arr,target,index+1);
+        list.addAll(answerFromBelowCalls);
+        return list;
     }
 }
