35f4f961fdbb39265db0bae10485a05f01c9a6c4
parshva555 <parshvadani@gmail.com>
2023-12-18 16:00:33 +0530

    Linear search with index

--- Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
+++ Recursion/src/RecursionInArrays/LinearSearchUsingRec.java
@@ -3,13 +3,25 @@ package RecursionInArrays;
 public class LinearSearchUsingRec {
     public static void main(String[] args) {
         int[] arr = {123,3452,3,54,23};
-        int target = 123;
-        System.out.println(find(arr,target,0));
+        int target = 23;
+        int ans  = findIndex(arr,target,0);
+        System.out.println(ans);
     }
     static boolean find(int [] arr, int target, int index){
         if(index == arr.length){
             return false;
         }
         return arr[index] == target || find(arr,target,index+1);
+    }
+    static int findIndex(int [] arr, int target , int index){
+        if(index == arr.length){
+            return -1;
+        }
+        if(arr[index] == target) {
+            return index;
+        }
+        else{
+            return findIndex(arr,target,index+1);
+        }
     }
 }
