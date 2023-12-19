e0ce56712889ecc696c7c7b5478c515493fab8f7
parshva555 <parshvadani@gmail.com>
2023-12-19 16:59:02 +0530

    Reverse pattern

--- Recursion/src/patterns/patterns.java
+++ Recursion/src/patterns/patterns.java
@@ -3,6 +3,8 @@ package patterns;
 public class patterns {
     public static void main(String[] args) {
         traingle(4,0);
+        traingle2(4,0);
+
     }
     static void traingle (int r, int c){
         if(r == 0){
@@ -16,6 +18,21 @@ public class patterns {
             System.out.println();
             traingle(r-1,0);
         }
-
     }
+    static void traingle2(int r, int c){
+        if(r == 0){
+            return;
+        }
+        if(c < r){
+            traingle2(r,c+1);
+            System.out.print("*");
+            
+        }
+        else{
+            traingle2(r-1,0);
+            System.out.println();
+
+        }
+    }
+
 }
