20320ee2b0324caaf6d370fc1d96b40ecacd0646
parshva555 <parshvadani@gmail.com>
2023-12-19 16:56:03 +0530

    Triangle pattern using recursion

---
+++ Recursion/src/patterns/patterns.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class patterns {
+    public static void main(String[] args) {
+        traingle(4,0);
+    }
+    static void traingle (int r, int c){
+        if(r == 0){
+            return;
+        }
+        if(c < r){
+            System.out.print("*");
+            traingle(r,c+1);
+        }
+        else{
+            System.out.println();
+            traingle(r-1,0);
+        }
+
+    }
+}
