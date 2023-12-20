3a72297e66730e04dbac3819da4b52952b1e7e32
parshva555 <parshvadani@gmail.com>
2023-12-20 13:28:44 +0530

    Merge Sort in Place using recursion

--- Recursion/src/Sorting/MergeSortUsingRec.java
+++ Recursion/src/Sorting/MergeSortUsingRec.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MergeSortUsingRec {
     public static void main(String[] args) {
         int [] arr = {5,4,3,2,1,6};
-        arr = mergeSort(arr);
+        mergeSortInPlace(arr,0,arr.length);
         System.out.println(Arrays.toString(arr));
     }
     static int[] mergeSort(int [] arr){
@@ -46,5 +46,48 @@ public class MergeSortUsingRec {
             j++;k++;
         }
         return mix;
+    }
+
+
+    static void mergeSortInPlace(int [] arr,int s,int e){
+        if(e-s ==1){
+            return ;
+        }
+        int m = (s+e)/2;
+        mergeSortInPlace(arr,s,m);
+        mergeSortInPlace(arr,m,e);
+        mergeInPlace(arr,s,m,e);
+    }
+    private static void mergeInPlace(int[] arr,int s,int m ,int e){
+        int [] mix = new int[e-s];
+
+        int i = s;
+        int j =m;
+        int k =0;
+        while(i < m && j < e){
+            if(arr[i] < arr[j]){
+                mix[k] = arr[i];
+                i++;
+            }
+            else{
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while(i<m){
+            mix[k] = arr[i];
+            i++;k++;
+
+        }
+        while(j<e){
+            mix[k] = arr[j];
+            j++;k++;
+        }
+        for(int l=0;l<mix.length;l++){
+            arr[s+l] = mix[l];
+        }
     }
 }
